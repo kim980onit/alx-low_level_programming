9b87d6c897fe88982b30c0ba62ca9cff8b45355a
kim980onit <kimanikamu971@gmail.com>
2023-10-12 12:06:49 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,26 +2,24 @@
 
 
 /**
-  * main - the main printing funtion
-  * less than 4000000.
-  * Return: 0.
-  */
-
+* main - the main printing funtion
+* less than 4000000.
+* Return: 0.
+*/
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+int i = 0;
+long j = 1, k = 2, sum = k;
 
-
-	while (k + j < 4000000)
-	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
-	}
-	printf("%ld", sum);
-	return (0);
+while (k + j < 4000000)
+{
+k += j;
+if (k % 2 == 0)
+sum += k;
+j = k - j;
+++i;
+}
+printf("%ld\n", sum);
+return (0);
 }
