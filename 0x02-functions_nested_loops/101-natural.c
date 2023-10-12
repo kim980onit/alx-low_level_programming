ed8c745e6bc5b55c882d1d02a11a64d37b600fbd
kim980onit <114307576+kim980onit@users.noreply.github.com>
2023-10-12 21:51:34 +0300

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,13 +10,14 @@
 
 int main(void)
 {
-int sum = 0;
-int i;
-
-for (i = 0 ; i < 1024 ; i++)
+int i, sum = 0;
+while (i < 1024)
 {
 if (i % 3 == 0 || i % 5 == 0)
-sum = sum + i;
+sum += i;
+}
+{
+i++  
 }
 printf("%d\n", sum);
 return (0);
