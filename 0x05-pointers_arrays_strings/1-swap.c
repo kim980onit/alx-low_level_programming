a343218fc966c45bd4b3a4a6b52c378981837b2e
kim980onit <kimanikamu971@gmail.com>
2023-10-16 11:31:00 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a:firstparemer
+ * @:secondparemeter
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
