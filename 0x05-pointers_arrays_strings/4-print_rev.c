ff11e3f5d1b53492987af5facfcf7186b339607e
kim980onit <kimanikamu971@gmail.com>
2023-10-16 12:46:00 -0700

    4-print_rev
    git commit -m 4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string
+ * @s:parameter
+ */
+
+void print_rev(char *s)
+{
+int i;
+int count = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+for  (i = count - 1; i >= 0; i--)
+{
+	_putchar (s[i]);
+}
+	_putchar ('\n');
+}
