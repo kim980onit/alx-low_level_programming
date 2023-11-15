55e7c10df90f29f8c0ceb6784fc522951904c442
kim980onit <kimanikamu971@gmail.com>
2023-11-15 12:03:49 -0800

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr -function that concatenates all program arguments
+ * @ac:firstparemeter that count
+ * @av:secondparemete
+ *
+ * Return: the pointer to the array of char
+ */
+char *argstostr(int ac, char **av)
+{
+	char *aout;
+	int i, j, k = 0, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+	}
+	len += ac;
+
+	aout = malloc(sizeof(char) * len + 1);
+	if (aout == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			aout[k] = av[i][j];
+			k++;
+		}
+		if (aout[k] == '\0')
+		{
+			aout[k++] = '\n';
+		}
+	}
+	return (aout);
+}
