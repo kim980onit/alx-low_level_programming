58930204877300e5cac46a1f0280d843fdb7f8b4
kim980onit <kimanikamu971@gmail.com>
2023-11-16 10:08:25 -0800

    update 1.0 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,21 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+       int count = 0;
+       while (h)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 
-		nodes++;
+		count++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (count);
 }
