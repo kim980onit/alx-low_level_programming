710e1d427d798d69e090cbc356db5bcd1eca6b6b
kim980onit <kimanikamu971@gmail.com>
2023-11-16 10:21:48 -0800

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+* list_len -function that checks the code
+* @h:paremeter that lists name
+* Return: number of nodes.
+*/
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
