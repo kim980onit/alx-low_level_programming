15ce67b8eff956f33d0a021750c45387557f24dd
kim980onit <kimanikamu971@gmail.com>
2023-11-17 06:58:30 -0800

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+* _calloc -function that allocates memory for an array of @nmemb elements
+* @size bytes each and returns a pointer to the allocated memory.
+*
+* @nmemb:firstparemeter
+* @size:secondparemeter
+*
+* Return: pointer to the allocated memory.
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int b;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+
+	for (b = 0; b < (nmemb * size); b++)
+		a[b] = 0;
+	return (a);
+}
+
