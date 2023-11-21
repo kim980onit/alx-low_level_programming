e4e1aeb2219b7854c5e6ce704a3466c5533f7e94
kim980onit <kimanikamu971@gmail.com>
2023-11-21 03:37:34 -0800

    update 3-add

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -28,7 +28,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	while (temp->next)
 		temp = temp->next;
 
-		temp->next = new;
+	temp->next = new;
 
-		return (new);
+	return (new);
 }
